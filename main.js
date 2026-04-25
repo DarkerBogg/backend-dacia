c2261213b6a78e77f5ccc5ca3891131544b8fdbc
venuslora <venuslora420@gmail.com>
2026-04-25 08:11:55 +0300

    scraping db endpoints

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Sequelize, DataTypes } from 'sequelize';
 import * as prompts from './prompts/index.js';
 import { generate } from './gemini.js';
 import { UserInterests } from './db-instance.js';
@@ -10,11 +11,33 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
+const VALID_API_TOKEN = process.env.API_TOKEN;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/api', async (req, res) => {
+  const apiToken = req.headers['authorization'];
+  if (!apiToken || apiToken !== `Bearer ${VALID_API_TOKEN}`) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  const param = req.body.param;
+  try {
+const results = await UserInterests.findAll({
+  where: Sequelize.literal(`
+    EXISTS (
+      SELECT 1
+      FROM json_each(interests)
+      WHERE json_each.value IN (${param.map(i => `'${i}'`).join(",")})
+    )
+  `)
+}); res.json(results);
+  } catch (error) {
+    console.error('Error querying database:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.post('/city/:city', async (req, res) => {
   let city = req.params.city;
